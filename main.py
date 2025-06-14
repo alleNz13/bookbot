504c53b7f082b01313ed4232a87e7fd0b04239a0
alleNz13 <erkbilgin13@gmail.com>
2025-06-14 13:27:13 +0300

    C: shit

--- main.py
+++ main.py
@@ -7,17 +7,41 @@ def get_book_text (filepath):
     with open(filepath) as f:
         file_content = f.read()
     return file_content
+def dictionary_split_sort(dict_ass):
+    
+    small_dicts = []
+    for key, value in dict_ass.items():
+        small_dicts.append({key: value})
+
+    sortedlist = sorted(small_dicts, key=lambda item: list(item.values())[0],reverse=True)
+   
+    
+    return sortedlist
+        
+
+
 def main():
     filepath = "books/frankenstein.txt"
     content = get_book_text(filepath).split()
     strings = get_book_text(filepath).lower()
     word_num = get_word_number(content)
     char_nums = get_char_number(strings)
+    dict_list = dictionary_split_sort(char_nums)
+    
     
 
     #report
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}...")
+
+     print("============ BOOKBOT ============")
+     print(f"Analyzing book found at {filepath}...")
+     print("----------- Word Count ----------")
+     print(f"Found {word_num} total words")
+     print("--------- Character Count -------")
+     for character in dict_list:
+        
+         
+            
+     print("============= END ===============")
 
 
 
